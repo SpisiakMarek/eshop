631ae284dc1e8f0c80702cb1d8fbb241b7cbaee5
[Marek Spisiak] <[marek.spisiak165@gmail.com]>
2022-10-13 01:56:13 +0200

    dorobeny responzivny dizajn

--- src/pages/Men/MenClothing/MenClothing.js
+++ src/pages/Men/MenClothing/MenClothing.js
@@ -4,9 +4,9 @@ function MenClothing() {
   return (
     <div className="products-wrapper">
       <header className="products-header">
-        <button>ponozky</button>
-        <button>tricka</button>
-        <button>rifle</button>
+        <button className="products-header-button">ponozky</button>
+        <button className="products-header-button">tricka</button>
+        <button className="products-header-button">rifle</button>
       </header>
       <div className="products-browser-wraper">
         <div className="products-filter-wrapper">filter</div>
