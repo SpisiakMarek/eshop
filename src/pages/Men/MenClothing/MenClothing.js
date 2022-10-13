72d0d94e51379b8f901e9b3ec67e990c3fe070b2
[Marek Spisiak] <[marek.spisiak165@gmail.com]>
2022-10-13 07:30:28 +0200

    provizorny filter

--- src/pages/Men/MenClothing/MenClothing.js
+++ src/pages/Men/MenClothing/MenClothing.js
@@ -9,7 +9,100 @@ function MenClothing() {
         <button className="products-header-button">rifle</button>
       </header>
       <div className="products-browser-wraper">
-        <div className="products-filter-wrapper">filter</div>
+        <div className="products-filter-wrapper">
+          <b>SizeTops</b>
+          <ul className="filter-list">
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>M</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>L</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>XL</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>S</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>XXL</p>
+            </li>
+          </ul>
+          <b>SizeBottoms</b>
+          <ul className="filter-list">
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>M</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>L</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>XL</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>S</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>XXL</p>
+            </li>
+          </ul>
+          <b>SizeTrunks</b>
+          <ul className="filter-list">
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>M</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>L</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>XL</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>S</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>XXL</p>
+            </li>
+          </ul>
+          <b>Brands</b>
+          <ul className="filter-list">
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>Nike</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>Jordan</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>Vans</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>Converse</p>
+            </li>
+            <li className="filter-item">
+              <input type="checkbox"></input>
+              <p>Calvin Klein</p>
+            </li>
+          </ul>
+        </div>
         <div className="products-list-wrapper">
           <Product />
           <Product />
